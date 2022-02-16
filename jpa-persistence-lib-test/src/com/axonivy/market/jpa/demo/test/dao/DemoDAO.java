c2dd325bc5ac3380305ecb1f0184cf00912773cb
Majed Hmamad <majed.hmamad@axonivy.com>
2022-02-16 16:53:52 +0100

    MARKET-216 JPA Persistence Lib - Step 1 JPA Demo

--- jpa-persistence-lib-test/src/com/axonivy/market/jpa/demo/test/dao/DemoDAO.java
+++ jpa-persistence-lib-test/src/com/axonivy/market/jpa/demo/test/dao/DemoDAO.java
@@ -8,7 +8,7 @@ import org.dbunit.DatabaseUnitException;
 import com.axonivy.market.jpa.demo.daos.BaseDAO;
 
 public class DemoDAO extends TestDAO implements BaseDAO {
-	private static final String TESTDATA_RESOURCE_PATH = "/ch/ivyteam/demo/jpa/demos/test/testdata/testdata.xlsx";
+	private static final String TESTDATA_RESOURCE_PATH = "/com/axonivy/market/jpa/demo/test/testdata/testdata.xlsx";
 	private static final DemoDAO instance = new DemoDAO();
 
 	protected DemoDAO() {
